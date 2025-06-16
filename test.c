ef335e26ad2c8cb012fb05c251f4cf5f44296271
Ruan C. Keet <rckeet@gmail.com>
2025-06-16 12:14:20 +0200

    Corrected function call

--- test.c
+++ test.c
@@ -4,7 +4,7 @@
 #define PI 3.14159265359f
 
 int main(void) {
-    const vec4_t v0 = rkm_vec4_new_single(PI);
+    const vec4_t v0 = rkm_vec4_fill(PI);
     const vec4_t v1 = rkm_vec4_new(3.0f, 2.0f, 4.0f, 5.0f);
     const vec4_t v2 = rkm_vec4_max(v0, v1);
 
